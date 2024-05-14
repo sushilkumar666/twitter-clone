95740aea6384b137398e93190b73d0fd3faa7bd6
sushilkumar666 <sushilssharma064@gmail.com>
2024-05-14 16:57:33 +0530

    final-commit

--- frontend/twitterclone/src/utils/constant.js
+++ frontend/twitterclone/src/utils/constant.js
@@ -1,5 +1,5 @@
-export const USER_API_END_POINT = "https://twitter-clone-green-ten.vercel.app/api/v1/user";
-export const TWEET_API_END_POINT = "https://twitter-clone-green-ten.vercel.app/api/v1/tweet";
+export const USER_API_END_POINT = "http://localhost:8000/api/v1/user";
+export const TWEET_API_END_POINT = "http://localhost:8000/api/v1/tweet";
 
 
 
